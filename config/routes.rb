fb2ac98f065d8cb1808a6bd77e23f1f8d7d4609f
alexrdavis12 <alexrdavis12@gmail.com>
2016-03-31 16:07:44 +0000

    trying to route properly

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rottenpotatoes::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
-  root 'movies#index'
+  #root 'movies#index'
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
@@ -52,8 +52,4 @@ Rottenpotatoes::Application.routes.draw do
   # root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id(.:format)))'
 end
